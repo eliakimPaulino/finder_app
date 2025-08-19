e86f0ff42689dff1ea97991964d1dacf4ea7db2e
eliakim <eliakim.paulino@gmail.com>
2025-08-19 00:01:43 -0300

    feat: add profile image asset and update pubspec.yaml to include profile images directory

--- lib/features/main_page/presentation/main_page_presentation.dart
+++ lib/features/main_page/presentation/main_page_presentation.dart
@@ -30,58 +30,87 @@ class SelectOptionPage extends StatelessWidget {
               : KSizes.iconLg;
 
           return Scaffold(
-            body: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(32),
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 600),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.2,
-                      ),
-                      Text(
-                        'Bem-vindo ao\nFinder',
-                        style: TextStyle(
-                          fontSize: titleFontSize,
-                          fontWeight: FontWeight.w600,
+            body: SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 600),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.2,
                         ),
-                      ),
-                      SizedBox(height: 50),
-                      Text(
-                        'Encontre o que precisa, de forma rápida e fácil',
-                        style: TextStyle(
-                          fontSize: subtitleFontSize,
-                          fontWeight: FontWeight.w400,
-                          color: KColors.darkGrey,
+                        Text(
+                          'Bem-vindo ao\nFinder',
+                          style: TextStyle(
+                            fontSize: titleFontSize,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: verticalSpacingMid),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          buildOptionCard(
-                            context,
-                            title: 'BIN',
-                            description: 'Encontrar\nDouble Bin',
-                            route: '/dtr-finder',
-                            iconColor: Colors.blue.shade700,
-                            textColor: KColors.textInformationPrimaryLight,
-                            iconSize: iconSize,
+                        SizedBox(height: 50),
+                        Text(
+                          'Encontre o que precisa, de forma rápida e fácil',
+                          style: TextStyle(
+                            fontSize: subtitleFontSize,
+                            fontWeight: FontWeight.w400,
+                            color: KColors.darkGrey,
                           ),
-                          buildOptionCard(
-                            context,
-                            title: 'PEP',
-                            description: 'Buscar\nOrientaçã PEP',
-                            route: '/pep-finder',
-                            iconColor: Colors.green.shade700,
-                            textColor: Colors.green.shade700,
-                            iconSize: iconSize,
+                        ),
+                        SizedBox(height: verticalSpacingMid),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            buildOptionCard(
+                              context,
+                              title: 'BIN',
+                              description: 'Encontrar\nBin',
+                              route: '/dtr-finder',
+                              iconColor: Colors.blue.shade700,
+                              textColor: KColors.textInformationPrimaryLight,
+                              iconSize: iconSize,
+                            ),
+                            buildOptionCard(
+                              context,
+                              title: 'PEP',
+                              description: 'PEP',
+                              route: '/pep-finder',
+                              iconColor: Colors.green.shade700,
+                              textColor: Colors.green.shade700,
+                              iconSize: iconSize,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 60),
+                        Center(
+                          child: Row(
+                            mainAxisSize: MainAxisSize
+                                .min, // faz com que a Row só ocupe o espaço necessário
+                            children: [
+                              Text(
+                                'Desenvolvido por Eliakim',
+                                style: TextStyle(
+                                  fontSize: subtitleFontSize,
+                                  fontWeight: FontWeight.w400,
+                                  color: KColors.darkGrey,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 32,
+                              ), // espaço entre texto e avatar
+                              CircleAvatar(
+                                radius: 32, // tamanho do avatar
+                                backgroundImage: AssetImage(
+                                  'assets/images/profile/me.jpeg',
+                                ),
+                                backgroundColor: Colors.transparent,
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
